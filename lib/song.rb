bd2efa2c0a645cfdabede79b7f124940af641ed2
Dalya Eidda <dalia.eidda@gmail.com>
2018-12-30 21:32:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,9 +8,7 @@ class Song
   @@artists_count = {}
   
   def initialize (name, artist, genre) 
-    @name 
-    @artist
-    @genre
+    attr_accessor :name, :artist, :genre
     @@count += 1 
     @@artists 
     @@genres
